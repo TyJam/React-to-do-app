e1aabaa0bc023f2d2cffd38bc16cb75a7d5d85de
TyJam <djkoatz@gmail.com>
2020-05-20 21:38:02 -0400

    Updates

---
+++ precache-manifest.7cd9589302ca7f20b806fda7a39fc35e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6071e5250d59adb78e488cc9dc2d5c4",
+    "url": "/React-to-do-app/index.html"
+  },
+  {
+    "revision": "dc07a6fbc2cb48b0bbf8",
+    "url": "/React-to-do-app/static/css/2.eaa14dea.chunk.css"
+  },
+  {
+    "revision": "8a8e8cbf8271d4128d0b",
+    "url": "/React-to-do-app/static/css/main.69f7a627.chunk.css"
+  },
+  {
+    "revision": "dc07a6fbc2cb48b0bbf8",
+    "url": "/React-to-do-app/static/js/2.8ba88a27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-to-do-app/static/js/2.8ba88a27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a8e8cbf8271d4128d0b",
+    "url": "/React-to-do-app/static/js/main.cbab439d.chunk.js"
+  },
+  {
+    "revision": "942a8c865c17e169654b",
+    "url": "/React-to-do-app/static/js/runtime-main.4dc0c18d.js"
+  }
+]);
